4aca737060d1bf78de3f035f8f9f795765c51542
Cekiso <nonkululeko@quirky30.co.za>
2022-05-30 00:21:03 +0200

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const PgPromise = require("pg-promise")
 const express = require('express');
 const assert = require('assert');
 const fs = require('fs');
-
 require('dotenv').config()
 const API = require('./api');
 const { default: axios } = require('axios');
@@ -11,23 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-const config = {
-    connectionString: process.env.DATABASE_URL || 'postgres://nkully:nkully@localhost:5432/garment_app',
-    max: 30,
-    ssl: { rejectUnauthorized: false }
-};
-
-const db = pgp(config);
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://nkully:nkully@localhost:5432/garment_app';
 const pgp = PgPromise({});
 const db = pgp(DATABASE_URL);
 API(app, db);
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 5995;
 // API routes to be added here
 app.get('/', async function(req, res) {
     console.log(req.query)
 });
 app.listen(PORT, function() {
-
     console.log(`App started on port ${PORT}`)
 });
