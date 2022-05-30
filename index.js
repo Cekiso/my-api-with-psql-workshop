8dda780fc2c9c76b021da29a49094ccb867ddc86
Cekiso <nonkululeko@quirky30.co.za>
2022-05-31 00:39:20 +0200

    first commit

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://nkully:nkully@localhost:5432/garment_app';
+const config = {
+    connectionString: process.env.DATABASE_URL || 'postgres://your_username:your_password@localhost:5432/your_db_name',
+    max: 30,
+    ssl: { rejectUnauthorized: false }
+};
+
+// const db = pgp(config);
+// const DATABASE_URL = process.env.DATABASE_URL || 'postgres://nkully:nkully@localhost:5432/garment_app';
 const pgp = PgPromise({});
-const db = pgp(DATABASE_URL);
-API(app, db);
+// const db = pgp(DATABASE_URL);
+// API(app, db);
 const PORT = process.env.PORT || 5994;
 // API routes to be added here
 app.get('/', async function(req, res) {
